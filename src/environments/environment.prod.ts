1976ec5f32ac8a26184d7683f8b423d1642acb14
inr43 <107096913+inr43@users.noreply.github.com>
2023-01-05 00:29:02 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUserURL:"https://myloanapi.herokuapp.com/loan-api/v1/user/",
-  apiLoanURL:"https://myloanapi.herokuapp.com/loan-api/v1/loan/",
-  apiAccountURL:"https://myloanapi.herokuapp.com/loan-api/v1/account/",
-  apiLedgerURL:"https://myloanapi.herokuapp.com/loan-api/v1/ledger/"
+  apiUserURL:"https://13.115.154.253:3002/loan-api/v1/user/",
+  apiLoanURL:"https://13.115.154.253:3002/loan-api/v1/loan/",
+  apiAccountURL:"https://13.115.154.253:3002/loan-api/v1/account/",
+  apiLedgerURL:"https://13.115.154.253:3002/loan-api/v1/ledger/"
 };
